c8884f86ea66ef6c3be67ed1882a541fa3b8bce5
Christian-2003 <chst0211@gmail.com>
2025-12-16 17:26:23 +0100

    Removed unused DTO class #25

--- app/src/main/java/de/christian2003/chaching/application/analysis/dto/AnalysisDataSummary.kt
+++
@@ -1,7 +0,0 @@
-package de.christian2003.chaching.application.analysis.dto
-
-import de.christian2003.chaching.domain.type.Type
-
-data class AnalysisDataSummary(
-    val results: Map<Type, List<GroupedTypeSum>>
-)
