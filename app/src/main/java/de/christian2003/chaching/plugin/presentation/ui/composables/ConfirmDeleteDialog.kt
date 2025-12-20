ccc672aec2e558914c94fc538166325aae7733f5
Christian-2003 <chst0211@gmail.com>
2025-12-20 15:21:14 +0100

    Changes to ConfirmDeleteDialog.kt to better align with new designs

--- app/src/main/java/de/christian2003/chaching/plugin/presentation/ui/composables/ConfirmDeleteDialog.kt
+++ app/src/main/java/de/christian2003/chaching/plugin/presentation/ui/composables/ConfirmDeleteDialog.kt
@@ -1,14 +1,23 @@
 package de.christian2003.chaching.plugin.presentation.ui.composables
 
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import de.christian2003.chaching.R
 
 
@@ -27,48 +36,67 @@ fun ConfirmDeleteDialog(
     onConfirm: () -> Unit,
     options: ConfirmDeleteDialogOptions = ConfirmDeleteDialogOptions.Delete
 ) {
-    AlertDialog(
-        icon = {
-            Icon(
-                painter = painterResource(R.drawable.ic_delete),
-                contentDescription = ""
-            )
-        },
-        title = {
-            Text(
-                text = when(options) {
-                    ConfirmDeleteDialogOptions.Delete -> stringResource(R.string.confirm_delete)
-                    ConfirmDeleteDialogOptions.MoveToTrash -> stringResource(R.string.confirm_moveToTrash)
-                }
-            )
-        },
-        text = {
-            Text(text)
-        },
-        onDismissRequest = {
-            onDismiss()
-        },
-        confirmButton = {
-            TextButton(
-                onClick = onConfirm,
-                colors = ButtonDefaults.textButtonColors().copy(contentColor = MaterialTheme.colorScheme.error)
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Card(
+            shape = MaterialTheme.shapes.extraLarge
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+                    .padding(24.dp)
             ) {
+                //Title:
                 Text(
                     text = when(options) {
-                        ConfirmDeleteDialogOptions.Delete -> stringResource(R.string.button_delete)
-                        ConfirmDeleteDialogOptions.MoveToTrash -> stringResource(R.string.button_confirm)
-                    }
+                        ConfirmDeleteDialogOptions.Delete -> stringResource(R.string.confirm_delete)
+                        ConfirmDeleteDialogOptions.MoveToTrash -> stringResource(R.string.confirm_moveToTrash)
+                    },
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.headlineSmall
                 )
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = onDismiss
-            ) {
-                Text(stringResource(R.string.button_cancel))
+
+                //Text:
+                Text(
+                    text = text,
+                    modifier = Modifier.padding(
+                        top = 16.dp,
+                        bottom = 24.dp
+                    )
+                )
+
+                //Buttons:
+                FlowRow(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.align(Alignment.End)
+                ) {
+                    TextButton(
+                        onClick = onDismiss
+                    ) {
+                        Text(stringResource(R.string.button_cancel))
+                    }
+                    TextButton(
+                        onClick = onConfirm,
+                        colors = ButtonDefaults.textButtonColors().copy(
+                            contentColor = when(options) {
+                                ConfirmDeleteDialogOptions.Delete -> MaterialTheme.colorScheme.error
+                                ConfirmDeleteDialogOptions.MoveToTrash -> ButtonDefaults.textButtonColors().contentColor
+                            }
+                        )
+                    ) {
+                        Text(
+                            text = when(options) {
+                                ConfirmDeleteDialogOptions.Delete -> stringResource(R.string.button_delete)
+                                ConfirmDeleteDialogOptions.MoveToTrash -> stringResource(R.string.button_confirm)
+                            }
+                        )
+                    }
+                }
             }
         }
-    )
+    }
 }
 
 
